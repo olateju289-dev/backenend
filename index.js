ab2233c3532ed67bcc10dd6a4b1e744e1ce6ee1a
olateju289 <olateju289@gmail.com>
2026-04-23 21:16:09 +0100

    server

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get("/" , (request , response)=>{
 const users = [
   { id: 1, name: 'Alice', email: 'alice@example.com' },
   { id: 2, name: 'Bob',   email: 'bob@example.com'   },
+  { id: 3, name: 'Charlie', email: 'charlie@example.com' }
 ];
 
 // GET all users
